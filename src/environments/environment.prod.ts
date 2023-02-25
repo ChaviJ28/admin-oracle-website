f5826ca5b0c4c233136029269906649d1a40573e
Chavi Surujbhali <chavi.codevigor@gmail.com>
2023-02-25 16:37:58 +0400

    env fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  // production: true,
+  production: false,
+  apiUrl: "http://130.61.110.219:4550/",
+  appToken: "Dyweirr7sIDKymbF8gveYreT28p9xq23GQ1it66vgaxLytW4LxnR5Ohvn64qGPTprS7U6XzJ1426uJMVx9zCb6Uj9J1NEPuva4oKfLJxiUt9Poej8CLtTCh1E0o81izWt42"
 };
